aaca72edac4061236efba13bcf7449e69971d795
Pijan-py <mdhafizan30@gmail.com>
2024-10-08 11:50:09 +0800

    ubah total nanti pandai-pandai tengok sendiri

--- FYP/PJayS/shares/models.py
+++ FYP/PJayS/shares/models.py
@@ -1,19 +1,11 @@
 from django.db import models
-from student.models import Member
 from teacher.models import Teacher
-import uuid
+from django.utils.timezone import now
 
-class Saham(models.Model):
-    share_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)  # Unique identifier for each share entry
-    student = models.ForeignKey(Member, on_delete=models.CASCADE, null=True, blank=True)  # Link to student, if applicable
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=True, blank=True)  # Link to teacher, if applicable
-    share_amount = models.DecimalField(max_digits=10, decimal_places=2)  # Amount of shares (financial value)
-    share_date = models.DateField(auto_now_add=True)  # Date when the share was issued or updated
-    share_type = models.CharField(max_length=20, choices=[('Initial', 'Initial'), ('Additional', 'Additional')], default='Initial')  # Type of share
+class SahamTeacher(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    date_saham_added = models.DateField(default=now, editable=False)
 
     def __str__(self):
-        if self.teacher:
-            return f"Share for Teacher: {self.teacher.nama} - {self.share_amount} RM"
-        elif self.student:
-            return f"Share for Student: {self.student.nama} - {self.share_amount} RM"
-        return f"Share - {self.share_amount} RM"
+        return f"{self.teacher.nama} - {self.amount}"
--- FYP/PJayS/shares/urls.py
+++ FYP/PJayS/shares/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import share_page
+from .views import *
 
 urlpatterns = [
     path('home/share_page/', share_page, name='share_page'),
-
+    path('home/add_share_page/<int:teacher_id>/',  add_share_teacher_func, name='add_share_page'),
 ]
