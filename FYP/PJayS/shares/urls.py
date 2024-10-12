f350a28fffc635b38667c684357a3c0ae3f31fcd
Pijan-py <mdhafizan30@gmail.com>
2024-10-12 14:42:09 +0800

    change at saham page , pie, bar, and line graph is fucntioning

--- FYP/PJayS/shares/urls.py
+++ FYP/PJayS/shares/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('home/share_page/', share_page, name='share_page'),
-    path('home/add_share_page/<int:teacher_id>/',  add_share_teacher_func, name='add_share_page'),
+    path('home/add_share_page_teacher/<int:teacher_id>/', add_share_teacher_func, name='add_share_page_teacher'),
+    path('home/add_share_page_student/<str:member_id>/', add_share_student_func, name='add_share_page_student')
 ]
