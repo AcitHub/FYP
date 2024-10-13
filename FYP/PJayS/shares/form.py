edc36aa880aea99789fb106768fe53a9a56e979c
Pijan-py <mdhafizan30@gmail.com>
2024-10-13 08:34:49 +0800

    saham all done

--- FYP/PJayS/shares/form.py
+++ FYP/PJayS/shares/form.py
@@ -3,13 +3,11 @@ from django import forms
 from .models import SahamTeacher
 
 class Teacher_Share(forms.ModelForm):
-
     class Meta:
         model = SahamTeacher
         fields = ['amount']
 
 class Student_Share(forms.ModelForm):
-
     class Meta:
         model = SahamTeacher
         fields = ['amount']
