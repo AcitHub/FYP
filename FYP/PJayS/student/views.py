3027c3344385462763f19f03dbc51791eb16037b
rashid <tangansedikit04@gmail.com>
2024-11-03 08:38:36 +0800

    Merging Acit n idha file

--- FYP/PJayS/student/views.py
+++ FYP/PJayS/student/views.py
@@ -183,4 +183,10 @@ def update_student_kumpulan_page(request):
     member = Member.objects.all()
     return render(request, 'student/muka surat-pelajar-kemas kini-kumpulan.html', {'member': member})
 
+def help_page(request):
+    member = Member.objects.all()
+    return render(request, 'student/laman-perlukan bantuan.html', {'member': member})
 
+def profile_page(request):
+    member = Member.objects.all()
+    return render(request, 'student/laman-profil.html', {'member': member})
